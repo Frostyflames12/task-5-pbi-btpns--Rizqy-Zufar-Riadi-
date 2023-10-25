5205ced320a04a0f87ac038ec1f207923914845c
Frostyflames12 <rizqyriadi02@gmail.com>
2023-10-25 20:14:35 +0700

    updated validate
    return user instead of success message

--- middleware/requireAuth.go
+++ middleware/requireAuth.go
@@ -1,42 +1,53 @@
 package middleware
 
 import (
+	"example/goAPI/initializers"
+	"example/goAPI/models"
 	"fmt"
+	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 )
 
 func RequiredAuth(c *gin.Context) {
 
-	// tokenString, err := c.Cookie("Authorization")
+	tokenString, err := c.Cookie("Authorization")
 
-	// if err != nil {
-	// 	c.AbortWithStatus(http.StatusUnauthorized)
-	// }
+	if err != nil {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
 
-	// token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 
-	// 	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-	// 		return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-	// 	}
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
 
-	// 	return []byte(os.Getenv("SECRET")), nil
-	// })
+		return []byte(os.Getenv("SECRET")), nil
+	})
 
-	// if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 
-	// 	if float64(time.Now().Unix()) > claims["exp"].(float64) {
-	// 		c.AbortWithStatus(http.StatusUnauthorized)
-	// 	}
+		if float64(time.Now().Unix()) > claims["exp"].(float64) {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
 
-	// 	c.Next()
+		var user models.User
+		initializers.DB.First(&user, claims["sub"])
 
-	// 	fmt.Println(claims["foo"], claims["nbf"])
-	// } else {
-	// 	c.AbortWithStatus(http.StatusUnauthorized)
-	// }
+		if user.ID == 0 {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
 
-	fmt.Println("in middleware")
+		c.Set("user", user)
+		c.Next()
 
-	c.Next()
+		fmt.Println(claims["foo"], claims["nbf"])
+	} else {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+
 }
