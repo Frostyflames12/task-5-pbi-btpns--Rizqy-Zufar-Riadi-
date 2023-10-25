29672768360781db7a836d87f13ccd0864be57bb
Frostyflames12 <rizqyriadi02@gmail.com>
2023-10-25 20:06:44 +0700

    Added Login with jwt
    added simple login with jwt and cookies

--- controllers/usersController.go
+++ controllers/usersController.go
@@ -4,8 +4,11 @@ import (
 	"example/goAPI/initializers"
 	"example/goAPI/models"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -51,4 +54,71 @@ func Signup(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{})
+}
+
+func Login(c *gin.Context) {
+
+	//get email and password
+
+	var body struct {
+		Email    string
+		Password string
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body",
+		})
+
+		return
+	}
+
+	var user models.User
+	initializers.DB.First(&user, "email = ?", body.Email)
+
+	if user.ID == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid Email or Password",
+		})
+
+		return
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid Email or Password",
+		})
+
+		return
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Fail to create token",
+		})
+
+		return
+	}
+
+	c.SetSameSite(http.SameSiteLaxMode)
+	c.SetCookie("Authorization", tokenString, 3600*24*30, "", "", false, true)
+
+	c.JSON(http.StatusOK, gin.H{})
+
+}
+
+func Validate(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Successfully logged in!",
+	})
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"example/goAPI/controllers"
 	"example/goAPI/initializers"
+	"example/goAPI/middleware"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,5 +18,7 @@ func main() {
 	r := gin.Default()
 
 	r.POST("/signup", controllers.Signup)
+	r.POST("/login", controllers.Login)
+	r.GET("/validate", middleware.RequiredAuth, controllers.Validate)
 	r.Run()
 }
